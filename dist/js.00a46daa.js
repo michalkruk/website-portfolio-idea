2d7b9f128b80bf6e481d9bf1c16b6d7d7932781c
michalkruk <michal.k.kruk@gmail.com>
2019-07-06 21:03:48 +0200

    bug fix, background

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -16066,7 +16066,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61544" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62356" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
